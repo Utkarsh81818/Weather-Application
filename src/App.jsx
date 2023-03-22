0987ccde483fe5166a9bf853b3b1c99564d92ada
Utkarsh Mishra <UtkarshM@emids.com>
2023-03-22 12:49:12 +0530

    Weather Application Assignment from  SS solutions

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Temp from "./Weather/Temp";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Temp />
+    </React.Fragment>
+  );
+};
+
+export default App;
---
+++ src/Weather/Temp.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react";
+import "./Style.css";
+import WeatherCard from "../weather/WeatherCard";
+
+const Temp = () => {
+  const [searchValue, setSearchValue] = useState("Lucknow");
+  const [tempInfo, setTempInfo] = useState({});
+
+  const getWeatherInfo = async () => {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=fdd1ea6d0cb232affceeef08b4d7481b`;
+      const res = await fetch(url);
+      const data = await res.json();
+
+      const { temp, humidity, pressure } = data.main;
+      const { main: weathermood } = data.weather[0];
+      const { name } = data;
+      const { speed } = data.wind;
+      const { country, sunset } = data.sys;
+
+      const myNewWeatherInfo = {
+        temp,
+        humidity,
+        pressure,
+        weathermood,
+        name,
+        speed,
+        country,
+        sunset,
+      };
+
+      setTempInfo(myNewWeatherInfo);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getWeatherInfo();
+  }, []);
+
+  return (
+    <>
+      <div className="wrap">
+      <h1 style={{color:"white"}}>Weather Application</h1>
+        <div className="search">
+          <input
+            type="search"
+            placeholder="search..."
+            autoFocus
+            id="search"
+            className="searchTerm"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+          <button
+            className="searchButton"
+            type="button"
+            onClick={getWeatherInfo}
+          >
+            Search
+          </button>
+        </div>
+      </div>
+      {/*  our temp card */}
+      <WeatherCard tempInfo={tempInfo} />
+    </>
+  );
+};
+
+export default Temp;
